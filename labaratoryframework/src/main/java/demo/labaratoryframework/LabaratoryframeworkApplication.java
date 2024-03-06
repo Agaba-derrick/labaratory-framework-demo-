3894f486cbd517795a71f9867e7e6d1ce4eafbe1
agabaderrick <agabaderrick18@gmail.com>
2024-03-06 09:47:07 +0300

    innitial setup

---
+++ labaratoryframework/src/main/java/demo/labaratoryframework/LabaratoryframeworkApplication.java
@@ -0,0 +1,13 @@
+package demo.labaratoryframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabaratoryframeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabaratoryframeworkApplication.class, args);
+	}
+
+}
---
+++ labaratoryframework/src/test/java/demo/labaratoryframework/LabaratoryframeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.labaratoryframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabaratoryframeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
